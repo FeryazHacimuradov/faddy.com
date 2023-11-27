aedd56726088fc749f501422f0c9cbd752de08fb
“FeryazHacimuradov” <tu201906125@code.edu.az>
2023-11-28 00:50:11 +0400

    created login page and needed to fixing

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -5,8 +5,12 @@ export const Login = () => {
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Welcome to Faddy login page!</h1>
-          <p>Faddy faddy faddy</p>
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
           <span>Don't you have an account?</span>
           <button>Register</button>
         </div>
