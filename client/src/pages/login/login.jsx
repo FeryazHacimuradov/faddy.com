24fe9557c704871a1908391653afa8e75f4787e3
“FeryazHacimuradov” <tu201906125@code.edu.az>
2023-11-27 23:32:35 +0400

    creating a layout for login page

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,5 +1,26 @@
 import "./login.scss";
 
-export const login = () => {
-  return <div className="login">login</div>;
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome to Faddy login page!</h1>
+          <p>Faddy faddy faddy</p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Login;
