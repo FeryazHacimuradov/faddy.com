0c31cec7a82440337440674d48fbe7a603b01735
“FeryazHacimuradov” <tu201906125@code.edu.az>
2023-11-28 12:16:42 +0400

    created simple Registr form

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,11 +1,11 @@
 import "./register.scss";
 
-export const Register = () => {
+const Register = () => {
   return (
-    <div className="login">
+    <div className="register">
       <div className="card">
         <div className="left">
-          <h1>Hello World.</h1>
+          <h1>Hello!</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
