4d239ce7cf517b6884535baf1f2ce8d2e46ea408
“FeryazHacimuradov” <tu201906125@code.edu.az>
2023-12-06 15:50:36 +0400

    created Controllers file and rename routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
 const app = express();
+
+const userRoutes = require("./routes/userRoutes"); // Corrected import
+
+app.use("/api/users", userRoutes);
 
 app.listen(8800, () => {
   console.log("API working...");
